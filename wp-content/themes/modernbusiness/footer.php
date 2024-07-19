05ee800850aee4d19dcf0fd55330c6a65d9ffbfa
abdulhannandanish <mrahdanish@gmail.com>
2024-07-19 18:26:42 +0500

    working on footer.php

--- wp-content/themes/modernbusiness/footer.php
+++ wp-content/themes/modernbusiness/footer.php
@@ -14,9 +14,6 @@
                 </div>
             </div>
         </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/scripts.js"></script>
+        <?php wp_footer(); ?> 
     </body>
 </html>
--- wp-content/themes/modernbusiness/functions.php
+++ wp-content/themes/modernbusiness/functions.php
@@ -6,6 +6,11 @@ add_theme_support('title-tag');
 add_action('wp_enqueue_scripts', 'mb_scripts');
 function mb_scripts()
 {
+    // CSS
     wp_enqueue_style('mb-bootstrap-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css', array(), '1.0');
     wp_enqueue_style('mb-styles', get_theme_file_uri('/css/styles.css'), array('mb-bootstrap-icons'), '1.0');
+    // JS
+    // The Purpose of true word is to load in footer
+    wp_enqueue_script('mb-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array(), '1.0', true);
+    wp_enqueue_script('mb-scripts', get_theme_file_uri('/js/scripts.js'), array('mb-bootstrap'), '1.0', true);
 }
